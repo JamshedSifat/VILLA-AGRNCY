11cfb8fb326d27118b2f9b0de6e2e6e865f59a69
Jamshed Sifat <mdjamshedsifat@gmail.com>
2025-02-03 17:33:48 +0600

    third commit

---
+++ next.js
@@ -0,0 +1,6 @@
+<script>
+  function toggleContent() {
+    const content = document.getElementById('content');
+    content.style.display = content.style.display === 'none' || content.style.display === '' ? 'block' : 'none';
+  }
+</script>
